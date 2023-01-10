441a9da3281e79ea2223e20d1d2d6a1c98af63d8
manish bisht <manishbisth9711@gmail.com>
2023-01-10 19:46:51 +0530

    feat: footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import logo from "../../images/icons/logo.svg";
 
 const Footer = () => {
   return (
     <footer>
-      i am footer
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae pariatur
-        quisquam deleniti mollitia. Quasi cupiditate ab nihil incidunt, eligendi
-        aliquid deleniti dignissimos dolor? Itaque, perspiciatis. Aspernatur eum
-        dolor voluptatibus labore.
-      </p>
+      <div className="foot">
+        <img src={logo} alt="" />
+        <div>
+          <address>
+            345 Faulconer Drive, Suite 4 • Charlottesville, CA, 12345
+          </address>
+          <div>
+            <a href="tel:+91">1234567899</a>
+            <a href="tel:+91">1234567899</a>
+          </div>
+          <div className="socials">
+            <p>Social Media</p>
+
+            <div className="icons"></div>
+          </div>
+        </div>
+      </div>
+      <div className="foot-links">
+        <ul>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Help</li>
+          <li>Privacy Policy</li>
+          <li>Disclaimer</li>
+        </ul>
+        <p>Copyright © 2020 Minimumlivingcost. All rights reserved</p>
+      </div>
     </footer>
   );
 };
