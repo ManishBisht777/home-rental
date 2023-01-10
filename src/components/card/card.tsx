2a7766dc4214d3034a9584791a774a4dfadc3f0f
manish bisht <manishbisth9711@gmail.com>
2023-01-10 11:13:24 +0530

    feat: card section

---
+++ src/components/card/card.tsx
@@ -0,0 +1,33 @@
+import p1 from "../../images/property/p1.svg";
+import bed from "../../images/icons/Bed.svg";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <img src={p1} alt="name of property" />
+      <div className="prop__info">
+        <p className="prop__name">
+          2578 Folsom street, san francisco, CA, 94110
+        </p>
+        <p className="prop__type">Private Room</p>
+        <p className="prop__price">$1200/month</p>
+        <div className="prop__features">
+          <div className="prop__feature">
+            <img src={bed} alt="no of beds" />
+            <p>4</p>
+          </div>
+          <div className="prop__feature">
+            <img src={bed} alt="no of beds" />
+            <p>4</p>
+          </div>
+          <div className="prop__feature">
+            <img src={bed} alt="no of beds" />
+            <p>4</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,6 +2,7 @@ import map from "../../images/map.svg";
 import search from "../../images/icons/search.svg";
 import feature from "../../images/feature.svg";
 import houseicon from "../../images/icons/house.svg";
+import Card from "../../components/card/card";
 
 const Home = () => {
   return (
@@ -31,17 +32,72 @@ const Home = () => {
       </section>
 
       <section className="feature">
-        <h2>minimum living cost takes care of everything</h2>
+        <h2>Minimum living cost takes care of everything</h2>
         <div className="feature__box">
-          <img src={feature} alt="" />
+          <img className="feature__img" src={feature} alt="" />
           <div className="feature__list">
-            <div className="feature">
+            <div className="feature__unique">
+              <div>
+                <img src={houseicon} alt="" />
+              </div>
+              <p>Pay as Little as possible!</p>
+            </div>
+            <div className="feature__unique">
+              <div>
+                <img src={houseicon} alt="" />
+              </div>
+              <p>Pay as Little as possible!</p>
+            </div>
+            <div className="feature__unique">
+              <div>
+                <img src={houseicon} alt="" />
+              </div>
+              <p>Pay as Little as possible!</p>
+            </div>
+            <div className="feature__unique">
+              <div>
+                <img src={houseicon} alt="" />
+              </div>
+              <p>Pay as Little as possible!</p>
+            </div>
+            <div className="feature__unique">
+              <div>
+                <img src={houseicon} alt="" />
+              </div>
+              <p>Pay as Little as possible!</p>
+            </div>
+            <div className="feature__unique">
               <div>
                 <img src={houseicon} alt="" />
               </div>
               <p>Pay as Little as possible!</p>
             </div>
           </div>
+        </div>
+      </section>
+
+      <section className="property">
+        <div className="property__heading">
+          <h2>List of Properties</h2>
+          <button>Viee More</button>
+        </div>
+        <div className="property__box">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+
+        <div className="pagination">
+          <button>prev</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>next</button>
         </div>
       </section>
     </main>
