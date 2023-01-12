1246201a4799d2deaf2d888c093a1f944779ddde
manish bisht <manishbisth9711@gmail.com>
2023-01-12 16:16:38 +0530

    feat: navbar responsive

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import logo from "../../images/icons/logo.svg";
 
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState<boolean>(false);
+
   return (
     <nav className="navbar">
-      <img src={logo} alt="home rental logo" />
-      <button className="hamburger">ham</button>
+      <img className="nav-logo" src={logo} alt="home rental logo" />
+      <button
+        aria-expanded={navOpen}
+        onClick={() => setNavOpen(!navOpen)}
+        className="hamburger"
+      >
+        ham
+      </button>
       <ul className="nav-links">
         <li>Home</li>
         <li>Landlords</li>
         <li>Tenants</li>
         <li>Contact Us</li>
       </ul>
+      <button onClick={() => setNavOpen(!navOpen)} className="close">
+        close
+      </button>
     </nav>
   );
 };
